de17773aab1a75c911a988ecdddd00858a2e1c32
gajndranath <gajendra.tripathi.me@gmail.com>
2024-07-27 13:40:29 +0530

    update theme

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -30,12 +30,12 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative  bg-white dark:bg-black justify-center w-full group",
+        "relative  bg-black justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-neutral-800  pointer-events-none" />
       <motion.div
         className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
