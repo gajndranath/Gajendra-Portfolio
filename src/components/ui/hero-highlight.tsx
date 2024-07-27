452988622809c51756ffa5aa47b4dae0f6c3ba15
gajndranath <gajendra.tripathi.me@gmail.com>
2024-07-27 13:37:51 +0530

    previous theme

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -30,14 +30,14 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative bg-black justify-center w-full group",
+        "relative  bg-white dark:bg-black justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300   pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
       <motion.div
-        className="pointer-events-none bg-dot-thick-indigo-500  absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+        className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
           WebkitMaskImage: useMotionTemplate`
             radial-gradient(
