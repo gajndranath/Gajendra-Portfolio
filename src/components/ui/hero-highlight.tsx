13984884e9e8212f2237978c9d40a3b00272a02f
gajndranath <gajendra.tripathi.me@gmail.com>
2024-07-27 13:24:30 +0530

    adjust dark mode

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -30,7 +30,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative  bg-white dark:bg-black justify-center w-full group",
+        "relative  bg-black justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
