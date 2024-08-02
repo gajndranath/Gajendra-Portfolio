2292578c2a64b0bdc1aac8bb22c7bf74e702eeac
gajndranath <gajendra.tripathi.me@gmail.com>
2024-08-02 22:12:45 +0530

    vercel bugs

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,12 @@ export default defineConfig({
   plugins: [
     react(),
     ViteImagemin({
-      gifsicle: {
-        optimizationLevel: 3,
-      },
-      optipng: {
-        optimizationLevel: 5,
-      },
-      pngquant: {
-        quality: [0.65, 0.9],
-      },
+      verbose: true,
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
       svgo: {
-        plugins: [{ removeViewBox: false }, { removeEmptyAttrs: true }],
+        plugins: [{ removeViewBox: false }, { removeEmptyAttrs: false }],
       },
     }),
   ],
