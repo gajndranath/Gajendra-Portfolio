b572e8c941fa0d43ae1ed7bf217906706462f129
gajndranath <gajendra.tripathi.me@gmail.com>
2024-08-02 21:59:26 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import ViteImagemin from "vite-plugin-imagemin";
+import ViteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   plugins: [
     react(),
-    /*     ViteImagemin({
-      // plugin options
-    }), */
+    ViteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+      },
+      optipng: {
+        optimizationLevel: 5,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+      },
+      svgo: {
+        plugins: [{ removeViewBox: false }, { removeEmptyAttrs: true }],
+      },
+    }),
   ],
 });
