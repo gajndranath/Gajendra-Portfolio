c8027994dab23327a1033f514612c2b655ce2ea9
gajndranath <gajendra.tripathi.me@gmail.com>
2024-08-02 21:52:09 +0530

    fixing vercel build log

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteImagemin from "vite-plugin-imagemin";
+// import ViteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteImagemin({
+    /*     ViteImagemin({
       // plugin options
-    }),
+    }), */
   ],
 });
